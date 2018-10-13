321e35f74d4f3bb9a5d1ed107fa80e4e1e389279
Graeme Coupar <grambo@grambo.me.uk>
2018-10-13 17:45:52 +0100

    Some tests

--- src/lib.rs
+++ src/lib.rs
@@ -32,22 +32,24 @@ use std::marker::PhantomData;
 ///
 /// A CommandIter that was reading the stdin of this Port would receive a
 /// Vec<u8> with the UTF-8 bytes `Hello!` in it.
-pub struct CommandIter<'a, T>
-{
+pub struct CommandIter<'a, T> {
     reader: &'a mut Read,
-    phantom: PhantomData<T>
+    phantom: PhantomData<T>,
 }
 
 impl<'a, T> CommandIter<'a, T> {
     pub fn from_reader(reader: &'a mut Read) -> CommandIter<'a, T> {
-        CommandIter { reader: reader, phantom: PhantomData }
+        CommandIter {
+            reader: reader,
+            phantom: PhantomData,
+        }
     }
 }
 
 impl<'a, T> Iterator for CommandIter<'a, T>
-where T: CommandParser
-    {
-
+where
+    T: CommandParser,
+{
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
@@ -70,11 +72,12 @@ where T: CommandParser
 /// a Port that is handling multiple commands concurrently. Feel free to make a
 /// PR to better support your use case if that is the case.
 pub trait CommandParser {
-    fn parse_command<>(buffer: Vec<u8>) -> Self;
+    fn parse_command(buffer: Vec<u8>) -> Self;
 }
 
 impl<T> CommandParser for T
-where T: DeserializeOwned
+where
+    T: DeserializeOwned,
 {
     fn parse_command(buffer: Vec<u8>) -> Self {
         serde_eetf::from_bytes(&buffer).expect("Deserialization Failed")
@@ -89,7 +92,8 @@ where T: DeserializeOwned
 /// Each message is expected to start with a 4 byte message size, as happens
 /// when you open an Erlang port in `{:packet, 4}` mode.
 pub fn receive<T>(reader: &mut Read) -> Option<T>
-where T: CommandParser
+where
+    T: CommandParser,
 {
     match reader.read_u32::<BigEndian>() {
         Ok(packet_size) => {
@@ -106,7 +110,6 @@ where T: CommandParser
             panic!("IO when reading size {}", err);
         }
     }
-
 }
 
 /// Writes an EETF Term into an io::Write along with it's size.
@@ -163,7 +166,7 @@ pub fn reply<R, T, E>(output: &mut Write, response: R)
 where
     R: Into<ErlResult<T, E>>,
     T: Serialize,
-    E: Serialize
+    E: Serialize,
 {
     send::<ErlResult<T, E>>(output, response.into());
 }
@@ -176,19 +179,13 @@ where
 /// 2. `{:error err}` on Error
 ///
 /// All replies sent via `reply` are converted into this enum.
-#[derive(Serialize)]
-pub enum ErlResult<T, E>
-where T: Serialize,
-      E: Serialize
-{
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub enum ErlResult<T, E> {
     Ok(T),
     Error(E),
 }
 
-impl<T, E> From<Result<T, E>> for ErlResult<T, E>
-where T: Serialize,
-      E: Serialize
-{
+impl<T, E> From<Result<T, E>> for ErlResult<T, E> {
     fn from(result: Result<T, E>) -> Self {
         match result {
             Ok(success) => ErlResult::Ok(success),
@@ -199,8 +196,67 @@ where T: Serialize,
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    use serde_eetf;
+    use std::fs::File;
+    use std::io::{BufReader, Cursor, Read};
+
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    struct TestCommand {
+        int8: i8,
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_command_iter() {
+        let buff = serde_eetf::to_bytes(&TestCommand { int8: 100 }).unwrap();
+
+        let mut size_buff = Vec::new();
+        size_buff
+            .write_u32::<BigEndian>(buff.len() as u32)
+            .expect("write data size failed");
+        size_buff.extend_from_slice(&buff);
+        size_buff
+            .write_u32::<BigEndian>(buff.len() as u32)
+            .expect("write data size failed");
+        size_buff.extend_from_slice(&buff);
+
+        let results: Vec<TestCommand> =
+            CommandIter::from_reader(&mut Cursor::new(size_buff)).collect();
+        assert_eq!(results.len(), 2);
+        assert_eq!(results[0], results[1]);
+    }
+
+    #[test]
+    fn test_reply() {
+        fn do_test(input: Result<i8, String>, expected_output: ErlResult<i8, String>) {
+            let mut cursor = Cursor::new(vec![]);
+            reply(&mut cursor, input);
+            cursor.set_position(0);
+
+            let result: Vec<ErlResult<i8, String>> =
+                CommandIter::from_reader(&mut cursor).collect();
+
+            assert_eq!(result, [expected_output]);
+        }
+
+        do_test(Ok(1), ErlResult::Ok(1));
+        do_test(
+            Err("Nope".to_string()),
+            ErlResult::Error("Nope".to_string()),
+        );
+    }
+
+    #[test]
+    fn test_send() {
+        let input = TestCommand { int8: 127 };
+
+        let mut cursor = Cursor::new(vec![]);
+        send(&mut cursor, &input);
+        cursor.set_position(0);
+
+        let result: Vec<TestCommand> = CommandIter::from_reader(&mut cursor).collect();
+
+        assert_eq!(result, [input]);
     }
 }
